d16ed0015d6a3d8d11173d244837362f534fe85e
chihpao <chihpaoo@gmail.com>
2025-09-18 16:48:34 +0800

    fix: 修正資源載入路徑問題

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/flownest/' : '/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
