d4e1a44c0f92220027467ec6f38004b04c117ecc
chihpao <chihpaoo@gmail.com>
2025-09-18 16:45:56 +0800

    修復空白頁面問題v2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/flownest/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
