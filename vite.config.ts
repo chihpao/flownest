36fa6599b2868a95a7a3abc90d98bfd7bd71bdc1
chihpao <chihpaoo@gmail.com>
2025-09-18 16:37:12 +0800

    準備部署到 Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,16 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   },
-  server: {
-    port: 3000
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
   }
 })
